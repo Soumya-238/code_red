e4e21a9f5bbfdf528e2ab052da026dfca6da0c1e
Soumya-238 <soumyasrivastava23@gmail.com>
2020-04-19 16:36:05 +0530

    SOS

---
+++ app/src/main/java/com/example/dell/code_red/sos.java
@@ -0,0 +1,13 @@
+package com.example.dell.code_red;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sos);
+    }
+}
